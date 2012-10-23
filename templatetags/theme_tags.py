ef7e66cfefe0ce7d9eb53a36a23994c302c5c456
James O'Neill <hemebond@gmail.com>
2012-10-23 17:06:16 +1300

    Refactor pages.

--- templatetags/theme_tags.py
+++ templatetags/theme_tags.py
@@ -1,10 +1,8 @@
 from django import template
 from django.core.urlresolvers import reverse
-from django.forms.widgets import Media
-from django.forms import Form
-from django.template import RequestContext
-import bleach
 from django.conf import settings
+
+import bleach
 
 register = template.Library()
 
@@ -21,3 +19,48 @@ def clean(value):
 		Runs the value through Bleach.
 	"""
 	return bleach.clean(value, tags=settings.BLEACH_ALLOWED_TAGS)
+
+
+@register.filter
+def get(value, arg):
+	print arg
+	field = getattr(value, arg)
+	print dir(field)
+	print type(field)
+	try:
+		f = "get_%s_display" % arg
+		fun = getattr(value, f)
+		return fun()
+	except:
+		pass
+
+	try:
+		return "%s (b)" % getattr(value, arg)
+	except:
+		return ""
+
+@register.tag
+def datatable(parser, token):
+	tokens = token.split_contents()[1:]
+	object_list = tokens[0]
+	object_fields = tokens[1:]
+
+	print object_list
+	print object_fields
+
+	return DatatableNode(object_list, object_fields)
+
+class DatatableNode(template.Node):
+	def __init__(self, object_list_var, object_fields):
+		self.object_list_var = object_list_var
+		self.object_fields = object_fields
+
+	def render(self, context):
+		object_list = context[self.object_list_var]
+		object_fields = self.object_fields
+		object_field_names = [
+			object_list.model._meta.get_field(f).verbose_name
+			for f
+			in object_fields
+		]
+		return "<p>%s</p>" % ",".join(object_field_names)
